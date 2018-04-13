bb324feccb05db749c299880a609dc164ffc05d1
Thomas Barnå <thomas@debian.enea.com>
2018-04-13 19:01:58 +0200

    kmom02/uppgift2 first commit

--- README.md
+++ README.md
@@ -2,6 +2,14 @@ My redovisa in oophp
 ====================
 
 This is a project repo for course oophp at BTH (bth.se).
-2018-04-06 Project init
-2018-04-08 Added report text, styling and bootstrap stylesheets.
+Author: thba17
+
+v2.0.1
+2018-04-09  Moved bootstrap stylesheet to Page.php
+            Added phpDocumentor
+            Built documentation
+
+v1.0.x
+2018-04-08  Added report text, styling and bootstrap stylesheets.
             kmom01 finished.
+2018-04-06 Project init
--- view/layout/oophp/default.php
+++ view/layout/oophp/default.php
@@ -23,7 +23,7 @@ namespace Anax\View;
 <?php endif; ?>
 
 <?php foreach ($stylesheets as $stylesheet) : ?>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
     <link rel="stylesheet" type="text/css" href="<?= asset($stylesheet) ?>">
 <?php endforeach; ?>
 
